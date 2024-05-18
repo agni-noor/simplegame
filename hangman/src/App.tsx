f4fa30a7a23161fbf54d405d99ed854b0d926002
Agni Noor <agninoor04@gmail.com>
2024-05-19 01:45:39 +0600

    hooks for guessing words added

--- hangman/src/App.tsx
+++ hangman/src/App.tsx
@@ -1,5 +1,17 @@
+import { useCallback, useEffect, useState } from "react"
+
+
+
+import words from "./wordList.json"
+
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
 
 function App() {
+  const [wordToGuess, setWordToGuess] = useState(getWord)
+ const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+  console.log(wordToGuess)
 
 
   return (
